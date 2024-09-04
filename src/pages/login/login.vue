39cc6ca940541db2216a85ad7b10ba6affa0d414
zhonglihao <14840436+zhonglihao@user.noreply.gitee.com>
2024-09-04 19:00:14 +0800

    20240904管理端第一次提交

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,11 +1,68 @@
 <script setup lang="ts">
 //
+import { ref } from 'vue'
+const activeLink = ref('商家登录')
+
+function setActiveLink(link: string) {
+  activeLink.value = link
+}
 </script>
 
 <template>
-  <view class="login">login</view>
+  <view class="login">
+    <view class="logo">
+      <text>logo</text>
+    </view>
+
+    <view class="identity">
+      <a
+        href=""
+        :class="{ active: activeLink === '商家登录' }"
+        @click.prevent="setActiveLink('商家登录')"
+        >商家登录</a
+      >
+      <text>|</text>
+      <a
+        href=""
+        :class="{ active: activeLink === '管理端登录' }"
+        @click.prevent="setActiveLink('管理端登录')"
+        >管理员登录</a
+      >
+    </view>
+    <button>登录</button>
+    <button>注册</button>
+  </view>
 </template>
 
 <style lang="scss">
 //
+.login {
+  font-size: 14px;
+}
+.logo {
+  width: 184px;
+  height: 184px;
+  margin: 20px auto;
+  background-color: aliceblue;
+}
+.identity {
+  margin: 0 auto;
+  width: 180px;
+}
+a {
+  margin-right: 10px;
+  margin-left: 10px;
+  color: #000;
+  text-decoration: none;
+  &.active {
+    text-decoration: underline;
+    text-underline-offset: 10px;
+  }
+}
+button {
+  margin: 20px auto;
+  width: 257px;
+  height: 45px;
+  border-radius: 5px;
+}
 </style>
---
+++ src/pages/login/login_1/login_1.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts"></script>
+<template>
+  <view class="body">
+    <view class="title">登录 </view>
+    <view class="input">
+      <view class="userid">
+        <text>账号</text>
+        <input type="text" placeholder="请输入账号/手机号" />
+      </view>
+      <view class="password">
+        <text>密码</text>
+        <input type="label" placeholder="请输入账号密码" />
+      </view>
+      <view class="forgetpassword">
+        <a href="">忘记密码?</a>
+      </view>
+      <view class="phonelogin">
+        <a href="">手机号一键登录</a>
+      </view>
+      <button>登录</button>
+      <view class="nouserid">
+        <a href="">还没有账号？去注册</a>
+      </view>
+      <view class="wxlogin">
+        <text>第三方登录方式</text>
+        <view class="img">
+          <i class="image"></i>
+        </view>
+        <text>微信</text>
+      </view>
+    </view>
+  </view>
+</template>
+
+<style scoped lang="scss">
+.body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+}
+
+.title {
+  font-size: 18px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+
+.input {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.userid,
+.password {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  margin-left: 50px;
+  input {
+    margin-left: 20px;
+    padding: 4px 0 3px 8px;
+    border: 1px solid #ccc;
+    background-color: #ccc;
+    border-radius: 8px;
+  }
+}
+
+.forgetpassword {
+}
+button {
+  margin: 20px auto;
+  width: 257px;
+  height: 45px;
+  border-radius: 5px;
+}
+</style>
