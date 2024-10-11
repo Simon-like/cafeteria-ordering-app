569e2fc9d649b69484e1ef92f04157d153869a39
zhonglihao <14840436+zhonglihao@user.noreply.gitee.com>
2024-10-11 12:25:17 +0800

    菜品管理

--- src/pages/admin_end/merchant_manage/content_view/detail.vue
+++ src/pages/admin_end/merchant_manage/content_view/detail.vue
@@ -27,49 +27,69 @@ const goBack = () => {
   <view class="container">
     <custom-nav-bar></custom-nav-bar>
     <view class="header">
-      <view class="back" @click="goBack">&lt;</view>
-      <view class="title">店铺详情</view>
+      <view class="back-btn" @click="goBack()"><i class="iconfont icon-zuojiantou"></i></view>
+      <view class="title">区域商家详细信息</view>
     </view>
-    <view class="content">
-      <view class="logo">
-        <image :src="detail.logo" class="logo"></image>
-      </view>
-      <view class="info">
-        <view class="name">{{ detail.name }}</view>
-        <view class="businessHours">营业时间: {{ detail.businessHours }}</view>
-        <view class="address">地址: {{ detail.address }}</view>
-        <view class="contactPhone">联系电话: {{ detail.contactPhone }}</view>
-        <view class="realName">所有人: {{ detail.realName }}</view>
-        <view class="description">店铺简介: {{ detail.description }}</view>
-      </view>
+
+    <view class="info">
+      <view class="info-item"
+        ><image :src="detail.logo" class="logo"></image>
+        <view class="name">店铺名称：{{ detail.name }}</view></view
+      >
+      <view class="info-item">营业时间: {{ detail.businessHours }}</view>
+      <view class="info-item">地址: {{ detail.address }}</view>
+      <view class="info-item">联系电话: {{ detail.contactPhone }}</view>
+      <view class="info-item">所有人: {{ detail.realName }}</view>
+      <view class="info-item">店铺简介: {{ detail.description }}</view>
+      <view class="info-item">店铺菜单:<navigator class="nav">点击跳转</navigator></view>
     </view>
-    <view class="menu">店铺菜单功能</view>
   </view>
 </template>
 
 <style lang="scss" scoped>
 .container {
-  display: flex;
-  flex-direction: column;
+  font-size: 30rpx;
   .header {
-    display: flex;
-    align-items: center;
-    padding: 20rpx;
-    .back {
-      margin-right: 20rpx;
+    height: 40rpx;
+    width: 100%;
+    .back-btn {
+      position: absolute;
+      left: 10rpx;
+      top: 60rpx;
+      padding: 10rpx;
+      background-color: rgba(0, 0, 0, 0.2);
+      border-radius: 16rpx;
+      font-weight: 550;
+      transition: 0.2s ease;
+      &:active {
+        scale: 0.9;
+      }
     }
-  }
-  .content {
-    display: flex;
-    .logo {
-      width: 200rpx;
-      height: 200rpx;
+    .title {
+      position: absolute;
+      top: 65rpx;
+      left: 70rpx;
+      font-size: 35rpx;
+      text-align: center;
     }
   }
   .info {
-    flex-grow: 1;
-    .menu {
+    .info-item {
       margin-top: 20rpx;
+      margin-bottom: 15rpx;
+      margin-left: 30rpx;
+      display: flex;
+      .logo {
+        width: 200rpx;
+        height: 200rpx;
+        background-color: #123;
+      }
+      .name {
+        font-size: 30rpx;
+      }
+      .nav {
+        text-decoration: underline;
+      }
     }
   }
 }
