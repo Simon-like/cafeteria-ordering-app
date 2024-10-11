a4f6a0b3bc9b48547a13f79ce4310ec060d760d3
zhonglihao <14840436+zhonglihao@user.noreply.gitee.com>
2024-10-11 20:22:10 +0800

    商家菜单管理（1）

--- src/pages/admin_end/merchant_manage/content_view/DishChange.vue
+++ src/pages/admin_end/merchant_manage/content_view/DishChange.vue
@@ -1,100 +1,145 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { ref } from 'vue'
+import iconComponent from '@/components/icons/icons.vue'
 
-const isExpanded = ref(false)
+// 状态管理，用于跟踪当前显示的视图
+const currentView = ref('main') // 初始视图为主页面
 
-const toggleExpand = () => {
-  isExpanded.value = !isExpanded.value
+const showAddDish = () => {
+  currentView.value = 'addDish' // 点击新增菜品时设置状态
 }
 
-const approve = () => {
-  console.log('通过')
+const showEditDish = () => {
+  currentView.value = 'editDish' // 点击更改菜品时设置状态
 }
 
-const reject = () => {
-  console.log('不通过')
+const goBack = () => {
+  currentView.value = 'main' // 返回主页面
 }
 </script>
+
 <template>
-  <view class="box">
-    <view class="content">
-      <view class="logo">
-        <image>logo</image>
-      </view>
-      <view class="info">
-        <view class="info-item"> 名称: </view>
-        <view class="info-item"> 地址: </view>
-        <view class="info-item"> 联系方式: </view>
-        <view class="else" v-show="isExpanded">
-          <view class="info-item"> 营业时间: </view>
-          <view class="info-item"> 所有人: </view>
-          <view class="info-item"> 店铺简介: </view>
-          <view class="btn"
-            ><button @click="approve">通过</button><button @click="reject">不通过</button></view
-          >
+  <view class="menu-manage">
+    <view class="box">
+      <!-- 主页面内容 -->
+      <view v-if="currentView === 'main'">
+        <view class="info">
+          <view class="logo"></view>
+          <view class="merchant">
+            <view class="name">
+              xxx
+              <navigator url="./detail" class="nav"> (点击跳转到商家信息页面) </navigator>
+            </view>
+            <view class="content">
+              <text>申请修改信息</text>
+              <view class="time">13:30</view>
+            </view>
+          </view>
         </view>
-        <i @click="toggleExpand" class="icon" :class="isExpanded ? 'upicon' : 'downicon'"></i>
+        <!-- 分割线 -->
+        <view class="divider"></view>
+
+        <view class="msg">
+          <view class="msg-item" @click="showAddDish">
+            新增菜品 <iconComponent :data="2" :radius="15"></iconComponent>
+          </view>
+          <view class="msg-item" @click="showEditDish">
+            更改已有菜品信息 <iconComponent :data="3" :radius="15"></iconComponent>
+          </view>
+        </view>
+      </view>
+
+      <!-- 新增菜品页面 -->
+      <view v-else-if="currentView === 'addDish'">
+        <button @click="goBack">返回</button>
+        <text>这里是新增菜品的页面内容</text>
+        <!-- 可以进一步添加表单等内容 -->
+      </view>
+
+      <!-- 更改菜品信息页面 -->
+      <view v-else-if="currentView === 'editDish'">
+        <button @click="goBack">返回</button>
+        <text>这里是更改菜品信息的页面内容</text>
+        <!-- 也可以添加表单或其他组件 -->
       </view>
     </view>
   </view>
 </template>
 
 <style lang="scss" scoped>
-.box {
+.menu-manage {
   width: 100%;
-  display: flex;
-  background-color: #ccc;
-  .content {
-    .logo {
-      background-color: #123;
-      margin-top: 10rpx;
-      margin-left: 10rpx;
-      width: 180rpx;
-      height: 180rpx;
-      object-fit: cover;
-    }
+  .box {
+    margin: 20rpx;
+    height: auto; /* 根据内容自适应高度 */
+    background-color: #ccc;
     .info {
-      width: 50%;
-      font-size: 30rpx;
-      position: absolute;
-      top: 170rpx;
-      left: 320rpx;
-      .info-item {
-        margin-left: 10rpx;
-        margin-bottom: 25rpx;
+      display: flex;
+      position: relative;
+      .logo {
+        margin: 20rpx;
+        height: 130rpx;
+        width: 130rpx;
+        border-radius: 50%;
+        background-color: #000;
       }
-      .else {
-        .btn {
-          width: 220rpx;
-          height: 45rpx;
+      .merchant {
+        margin-left: 15rpx;
+        margin-top: 20rpx;
+        display: flex;
+        flex-direction: column;
+        .name {
           display: flex;
-          margin-bottom: 20rpx;
-          button {
-            width: 90rpx;
-            padding: 0;
-            margin-right: 20rpx;
-            font-size: 20rpx;
-            border: 2rpx solid #000;
-            background-color: transparent; /* 设置背景色为透明 */
+          font-size: 35rpx;
+          .nav {
+            font-size: 25rpx;
+            color: #f5f5f5;
+            text-decoration: underline;
+          }
+        }
+        .content {
+          display: flex;
+          margin-top: 30rpx;
+          text {
+            font-size: 32rpx;
+            white-space: nowrap;
+          }
+          .time {
+            margin-left: 120rpx;
           }
         }
       }
-      .icon {
-        display: inline-block;
-        width: 40rpx;
-        height: 30rpx;
-        background-color: black;
-        clip-path: polygon(50% 0%, 100% 100%, 0% 100%);
-        transition: transform 0.3s ease;
-      }
+    }
 
-      .upicon {
-        transform: rotate(0deg); /* 向上箭头 */
-      }
+    .divider {
+      width: 60%;
+      height: 4rpx;
+      background-color: #000;
+      margin-left: 20%;
+      margin-bottom: 30rpx;
+    }
 
-      .downicon {
-        transform: rotate(180deg); /* 向下箭头 */
+    .msg {
+      margin: 20rpx;
+      .msg-item {
+        display: flex;
+        margin-bottom: 15rpx;
+        cursor: pointer; /* 鼠标悬停时显示为可点击 */
       }
+    }
+
+    button {
+      margin: 10rpx;
+      background-color: #007bff;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      padding: 10rpx;
+      cursor: pointer;
+    }
+
+    .dynamic-content {
+      margin-top: 20rpx; /* 动态内容与上方内容的间距 */
     }
   }
 }
