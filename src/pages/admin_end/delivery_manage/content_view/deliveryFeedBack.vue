c332cb4abbf303f17a44bcf1b2ca42982c324b46
zhonglihao <14840436+zhonglihao@user.noreply.gitee.com>
2024-11-02 21:38:27 +0800

    外卖员管理页面补充

--- src/pages/admin_end/delivery_manage/content_view/deliveryFeedBack.vue
+++ src/pages/admin_end/delivery_manage/content_view/deliveryFeedBack.vue
@@ -65,26 +65,41 @@ const hidePopup = () => {
 </script>
 <template>
   <view class="box">
-    <view v-for="info in tests" :key="info.id" class="card">
-      <view class="item">时间:{{ info.time }}</view>
-      <view class="item">类型:申述-{{ info.type }}</view>
-      <view class="item">内容:{{ info.content }}</view>
-      <button @click="handleButtonClick(info)">处理</button>
-    </view>
+    <scroll-view scroll-y="true" class="scroll-Y">
+      <view v-for="info in tests" :key="info.id" class="card">
+        <view class="item">时间:{{ info.time }}</view>
+        <view class="item">类型:申述-{{ info.type }}</view>
+        <view class="item">内容:{{ info.content }}</view>
+        <button @click="handleButtonClick(info)">处理</button>
+      </view>
+    </scroll-view>
 
     <uni-popup ref="popup" type="center">
       <view class="popup-content">
         <view class="content-item">时间: {{ selectedInfo.time }}</view>
         <view class="content-item">类型: {{ selectedInfo.type }}</view>
-        <view class="content-item">内容: {{ selectedInfo.content }}</view>
+        <view class="content-item">申述说明: {{ selectedInfo.content }}</view>
         <view class="content-item">上传附件: </view>
         <view class="content-item" style="text-decoration: underline"
           >相关订单:{{ selectedInfo.orderNumber }}</view
         >
-        <view class="content-item">内容: {{ selectedInfo.content }}</view>
+        <view class="content-item">
+          <view class="contactInfo"
+            ><text>外卖员:{{ selectedInfo.diliver }}</text
+            ><button class="contact">联系外卖员</button></view
+          >
+          <view class="contactInfo"
+            ><text>商家:{{ selectedInfo.name }}</text
+            ><button class="contact">联系商家</button></view
+          >
+          <view class="contactInfo"
+            ><text>用户:{{ selectedInfo.customer }}</text
+            ><button class="contact">联系用户</button></view
+          >
+        </view>
 
         <!-- 可以继续添加更多信息 -->
-        <button @click="hidePopup()">关闭</button>
+        <button @click="hidePopup()" class="close">关闭</button>
       </view>
     </uni-popup>
   </view>
@@ -93,7 +108,7 @@ const hidePopup = () => {
 <style lang="scss" scoped>
 .box {
   display: flex;
-  width: 100%;
+  width: 610rpx;
   flex-direction: column;
   .card {
     margin: 15rpx 15rpx 15rpx 15rpx;
@@ -116,13 +131,37 @@ const hidePopup = () => {
   }
 
   .popup-content {
+    display: flex;
+    flex-direction: column;
     background-color: #fff;
     margin-left: 120rpx;
     width: 500rpx;
     height: 1000rpx;
+    padding-top: 10rpx;
     .content-item {
       margin-top: 40rpx;
       margin-left: 15rpx;
+      display: flex;
+      flex-direction: column;
+
+      .contactInfo {
+        display: flex;
+        margin-right: auto;
+        margin-bottom: 40rpx;
+        button {
+          position: absolute;
+          font-size: 20rpx;
+          width: 200rpx;
+          height: 60rpx;
+          padding: 0;
+          right: 0;
+        }
+      }
+    }
+    .close {
+      margin-top: 50rpx;
+      width: 200rpx;
+      height: 80rpx;
     }
   }
 }
--- src/pages/admin_end/delivery_manage/content_view/deliveryInfo.vue
+++ src/pages/admin_end/delivery_manage/content_view/deliveryInfo.vue
@@ -1,5 +1,155 @@
-<template>Info</template>
+<script setup lang="ts">
+import { ref } from 'vue'
 
-<script setup lang="ts"></script>
+interface NoticeItem {
+  picture: string
+  name: string
+  phone: string
+  totalsalary: string
+  totalorder: string
+  A: string
+  B: string
+  C: string
+  is_show: boolean
+}
 
-<style lang="scss" scopped></style>
+const notice_list = ref<NoticeItem[]>([
+  {
+    picture: 'http://',
+    name: '张三',
+    phone: '123456789',
+    totalsalary: '1000',
+    totalorder: '100',
+    A: '40',
+    B: '30',
+    C: '30',
+    is_show: false,
+  },
+  {
+    picture: 'http://',
+    name: '李四',
+    phone: '987654321',
+    totalsalary: '1500',
+    totalorder: '150',
+    A: '50',
+    B: '40',
+    C: '40',
+    is_show: false,
+  },
+])
+
+const changeShow = (index: number) => {
+  notice_list.value[index].is_show = !notice_list.value[index].is_show
+}
+</script>
+
+<template>
+  <scroll-view scroll-y="true" class="scroll-Y">
+    <view class="info-box" v-for="(item, index) in notice_list" :key="index">
+      <view class="info">
+        <view class="logo">
+          <image :src="item.picture" mode="aspectFit"></image>
+        </view>
+        <view>
+          <view class="info-item">名称: {{ item.name }}</view>
+          <view class="info-item">联系方式: {{ item.phone }}</view>
+        </view>
+      </view>
+      <view class="info-wrapper" :class="{ show: item.is_show }">
+        <view class="content" v-if="item.is_show">
+          <view class="info-item">总收入: {{ item.totalsalary }}</view>
+          <view class="info-item">总订单: {{ item.totalorder }}</view>
+          <view class="info-item">指标A: {{ item.A }}</view>
+          <view class="info-item">指标B: {{ item.B }}</view>
+          <view class="info-item">指标C: {{ item.C }}</view>
+        </view>
+        <view @click="changeShow(index)" class="icon">
+          <i class="iconfont icon-jiantouarrow483"></i>
+        </view>
+      </view>
+    </view>
+  </scroll-view>
+</template>
+
+<style lang="scss" scoped>
+.info-box {
+  font-size: 25rpx;
+  padding: 20rpx;
+  display: flex;
+  flex-direction: column;
+  margin: 15rpx 15rpx 15rpx 15rpx;
+  background-color: rgba(0, 0, 0, 0.2);
+
+  .info {
+    display: flex;
+    align-items: center;
+
+    .logo {
+      width: 150rpx;
+      height: 150rpx;
+      border-radius: 50%;
+      margin-right: 20rpx;
+      background-color: #fff;
+      image {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+
+    .info-item {
+      margin-top: 10rpx;
+      margin-bottom: 10rpx;
+    }
+  }
+
+  .info-wrapper {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin-top: 0;
+
+    .content {
+      display: grid;
+      grid-template-rows: 0fr;
+      transition: all 0.3s ease-out;
+      border-top: 1px solid transparent;
+
+      .info-item {
+        margin-left: 170rpx;
+        margin-bottom: 10rpx;
+        overflow: hidden;
+      }
+
+      .btn {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        margin-top: 20rpx;
+        .approve-btn,
+        .reject-btn {
+          border: 2px solid black;
+          padding: 10rpx;
+          margin: 0 20rpx;
+        }
+      }
+    }
+
+    .icon {
+      margin: 0 auto;
+      text-align: center;
+      transition: 0.3s ease-out;
+    }
+
+    &.show {
+      .icon {
+        transform: rotate(180deg);
+      }
+
+      .content {
+        grid-template-rows: 1fr;
+      }
+    }
+  }
+}
+</style>
